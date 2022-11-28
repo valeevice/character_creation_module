519664b16464e2ed765eff12a43a071e1c75b3e2
Алесей Валеев <valeevice@mail.ru>
2022-11-28 23:22:30 +0300

    Make visualisation, and add docs

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from random import randint
 
+from graphic_arts.start_game_banner import run_screensaver
+
 
 def attack(char_name: str, char_class: str) -> str:
+    """Return the pathname of the KOS root directory."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику '
                 f'равный {5 + randint(3, 5)}')
@@ -14,6 +17,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Return the pathname of the KOS root directory."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -23,6 +27,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Return the pathname of the KOS root directory."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -33,6 +38,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Return the pathname of the KOS root directory."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -57,11 +63,12 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Return the pathname of the KOS root directory."""
     approve_choice: str = None
     char_class: str = None
     while approve_choice != 'y':
-        char_class = input('Введи название персонажа, за которого',
-                           'хочешь играть: Воитель — warrior, Маг — mage,',
+        char_class = input('Введи название персонажа, за которого ',
+                           'хочешь играть: Воитель — warrior, Маг — mage, ',
                            'Лекарь — healer: ')
         if char_class == 'warrior':
             print('Воитель — дерзкий воин ближнего боя.',
@@ -72,13 +79,13 @@ def choice_char_class() -> str:
         if char_class == 'healer':
             print('Лекарь — могущественный заклинатель. Черпает',
                   'силы из природы, веры и духов.')
-        approve_choice = input('Нажми (Y), чтобы подтвердить выбор,',
-                               'или любую другую кнопку, чтобы выбрать',
-                               'другого персонажа ').lower()
+        approve_choice = input(('Нажми (Y), чтобы подтвердить выбор, ',
+                                'или любую другую кнопку, чтобы выбрать ',
+                                'другого персонажа ')).lower()
     return char_class
 
-
 def main():
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -90,4 +97,5 @@ def main():
     print(start_training(char_name, char_class))
 
 
-main()
+if __name__ == '__main__':
+    main()
